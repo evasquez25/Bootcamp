76b02c921e4a4a3d7a98f8a440235a31c0e42804
Eric Vasquez <evasquezreyes@college.harvard.edu>
2024-11-08 08:59:09 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          READY TO GET STARTED YEAH!
         </p>
         <a
           className="App-link"
